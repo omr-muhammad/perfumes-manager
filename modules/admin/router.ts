f852da6fea16e6525c933bb771f759b1059d27a3
Omar <omarelsayedmuhammad@gmail.com>
2026-04-22 01:07:19 +0200

    add: Handled admin work in compnaies module

--- modules/admin/router.ts
+++ modules/admin/router.ts
@@ -1,12 +1,16 @@
 import Elysia from "elysia";
-import { userAdminRouter } from "../users/userAdminRouter";
 import { protect } from "../../utils/auth";
+import { userAdminRouter } from "../users/userAdminRouter";
 import { shopsAdminRouter } from "../shops/shopsAdminRouter";
+import { companiesAdminRouter } from "../companies/companiesAdminRouter";
 
 export const adminRouter = new Elysia({ prefix: "/admin" })
   .use(protect)
   .onBeforeHandle(({ authPayload, status }) => {
     if (authPayload.role !== "admin") return status(403);
   })
+  // done
+  .group("/companies", (app) => app.use(companiesAdminRouter))
+  // progress...
   .group("/users", (app) => app.use(userAdminRouter))
   .group("/shops", (app) => app.use(shopsAdminRouter));
