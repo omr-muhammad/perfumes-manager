db01e48c2a431a6fb2ac83a35c68d0499aa3f70a
Omar <omarelsayedmuhammad@gmail.com>
2026-05-04 23:00:29 +0300

    update: Appended the lot staff and Modified the alcohol

--- modules/inventory/alcohols/schema.ts
+++ modules/inventory/alcohols/schema.ts
@@ -1,5 +1,5 @@
 import { createInsertSchema } from "drizzle-typebox";
-import { alcoholsTable } from "../../../db/schema";
+import { alcoholLots, alcoholsTable } from "../../../db/schema";
 import { t, type Static } from "elysia";
 import {
   ID,
@@ -8,34 +8,53 @@ import {
   type Ctx,
 } from "../../../utils/globalSchema";
 
-const AlcoholDrivedSchema = createInsertSchema(alcoholsTable, {
-  ltBuyPrice: t.Number({ minimum: 0 }),
-  ltSellPrice: t.Number({ minimum: 0 }),
-  amountInMl: t.Number({ minimum: 0 }),
+const BaseAlco = createInsertSchema(alcoholsTable, {
   concentration: t.Optional(t.Number({ minimum: 1, maximum: 100 })),
+});
+const AlcoLot = createInsertSchema(alcoholLots, {
+  costPerLiter: t.Number({ minimum: 0 }),
+  baseSellPerLiter: t.Number({ minimum: 0 }),
   expiryDate: t.String(),
 });
 
-// -------------- Create --------------
-const CreateAlcoBody = t.Omit(AlcoholDrivedSchema, ["shopId", "unitSellPrice"]);
+// -------------- Create Alcohol --------------
+const Alcohol = t.Omit(BaseAlco, [
+  "shopId",
+  "unitSellPrice",
+  "createdAt",
+  "updatedAt",
+]);
+const AlcoholLot = t.Omit(AlcoLot, ["createdAt", "updatedAt", "alcoholId"]);
+type AlcoholLot = Static<typeof AlcoholLot>;
+
+const CreateAlcoBody = t.Object({
+  alcohol: Alcohol,
+  alcoholLot: AlcoholLot,
+});
 export type CreateAlcoBody = Static<typeof CreateAlcoBody>;
 
-// -------------- Update --------------
-const UpdateAlcoBody = t.Partial(CreateAlcoBody);
+// -------------- Update Alcohol --------------
+const UpdateAlcoBody = t.Partial(Alcohol);
 export type UpdateAlcoBody = Static<typeof UpdateAlcoBody>;
+
+// -- Update lot
+const UpdateLotBody = t.Partial(AlcoholLot);
+export type UpdateLotBody = Static<typeof UpdateLotBody>;
 
 // -------------- Query --------------
 const AlcoholsQueryFilters = t.Partial(
   t.Object({
+    // alcohols filters
     search: t.String(),
     type: t.String(),
-    minAmount: t.Number({ minimum: 0 }),
-    maxAmount: t.Number({ minimum: 0 }),
-    amountUnit: t.Union([t.Literal("ml"), t.Literal("l")]),
-    minLtPrice: t.Number({ minimum: 0 }),
-    maxLtPrice: t.Number({ minimum: 0 }),
     minConcentration: t.Number({ maximum: 100, minimum: 1 }),
     maxConcentration: t.Number({ maximum: 100, minimum: 1 }),
+
+    // lots filters
+    minAmount: t.Number({ minimum: 0 }),
+    maxAmount: t.Number({ minimum: 0 }),
+    minLtPrice: t.Number({ minimum: 0 }),
+    maxLtPrice: t.Number({ minimum: 0 }),
     expiresBefore: t.String(),
     expiresAfter: t.String(),
     ...QueriesMeta,
@@ -44,26 +63,44 @@ const AlcoholsQueryFilters = t.Partial(
 export type AlcoholQueryFilters = Static<typeof AlcoholsQueryFilters>;
 
 // -------------- URL Params --------------
-const AlcoInvParams = t.Object({
+const AlcoParams = t.Object({
   shopId: ID,
   alcoholId: ID,
 });
-type AlcoInvParams = Static<typeof AlcoInvParams>;
+type AlcoParams = Static<typeof AlcoParams>;
+
+const AlcoLotParams = t.Object({
+  shopId: ID,
+  alcoholId: ID,
+  lotId: ID,
+});
+type AlcoLotParams = Static<typeof AlcoLotParams>;
 
 // -------------- Alco Ctxs --------------
 export interface AlcoCTXs {
+  // alcohols
   createAlco: Ctx<CreateAlcoBody, ShopParams>;
-  updateAlco: Ctx<UpdateAlcoBody, AlcoInvParams>;
-  delAlco: Ctx<unknown, AlcoInvParams>;
+  updateAlco: Ctx<UpdateAlcoBody, AlcoParams>;
+  delAlco: Ctx<unknown, AlcoParams>;
   queryAll: Ctx<unknown, ShopParams>;
-  queryOne: Ctx<unknown, AlcoInvParams>;
+  queryOne: Ctx<unknown, AlcoParams>;
+
+  // alco lots
+  createAlcoLot: Ctx<AlcoholLot, AlcoParams>;
+  updateAlcoLot: Ctx<UpdateLotBody, AlcoLotParams>;
+  delAlcoLot: Ctx<unknown, AlcoLotParams>;
 }
 
 // -------------- Alco Schema --------------
 export const AlcoSchema = {
   create: { params: ShopParams, body: CreateAlcoBody },
   queryAll: { params: ShopParams, query: AlcoholsQueryFilters },
-  update: { params: AlcoInvParams, body: UpdateAlcoBody },
-  del: { params: AlcoInvParams },
-  queryOne: { params: AlcoInvParams },
+  update: { params: AlcoParams, body: UpdateAlcoBody },
+  del: { params: AlcoParams },
+  queryOne: { params: AlcoParams },
+
+  // Alco Lots
+  createLot: { params: AlcoParams, body: AlcoLot },
+  updateLot: { params: AlcoLotParams, body: UpdateLotBody },
+  delLot: { params: AlcoLotParams },
 };
