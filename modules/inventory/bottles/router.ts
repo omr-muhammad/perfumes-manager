1b568298ded67e9dff59daba5ce3f55f6ce82745
Omar <omarelsayedmuhammad@gmail.com>
2026-05-07 01:42:21 +0300

    update: Added routes for bottles lots

--- modules/inventory/bottles/router.ts
+++ modules/inventory/bottles/router.ts
@@ -2,13 +2,22 @@ import Elysia from "elysia";
 
 import { protect, restrictTo } from "../../../utils/auth";
 import * as handlers from "./handlers";
-import { AlcoSchema } from "./schema";
+import { BottleSchema } from "./schema";
 
 export const bottlesRouter = new Elysia({ prefix: "/bottles" })
   .use(protect)
   .use(restrictTo("owner"))
-  .get("", handlers.getShopBottles, AlcoSchema.qAll)
-  .post("", handlers.createBtl, AlcoSchema.create)
-  .get("/:bottleId", handlers.getBtlById, AlcoSchema.qOne)
-  .patch("/:bottleId", handlers.updateBtl, AlcoSchema.update)
-  .delete("/:bottleId", handlers.deleteBtl, AlcoSchema.del);
+  .get("", handlers.getShopBottles, BottleSchema.qAll)
+  .post("", handlers.createBtl, BottleSchema.create)
+  .group("/:bottleId", (app) =>
+    app
+      .get("", handlers.getBtlById, BottleSchema.qOne)
+      .patch("", handlers.updateBtl, BottleSchema.update)
+      .delete("", handlers.deleteBtl, BottleSchema.del)
+      .group("/lots", (app) =>
+        app
+          .post("", handlers.createBtlLot, BottleSchema.createLot)
+          .patch("/:lotId", handlers.updateBtlLot, BottleSchema.updateLot)
+          .delete("/:lotId", handlers.deleteBtlLot, BottleSchema.deleteLot),
+      ),
+  );
