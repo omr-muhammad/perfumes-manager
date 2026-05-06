c7965f478ebdd0d16a76c7df71407bd9985a4631
Omar <omarelsayedmuhammad@gmail.com>
2026-05-07 01:41:24 +0300

    update: Modified services params in handlers & Appended bottle lots handlers

--- modules/inventory/bottles/handlers.ts
+++ modules/inventory/bottles/handlers.ts
@@ -1,66 +1,94 @@
-import type { AlcoCTXs } from "./schema";
+import type { BottleCTXs } from "./schema";
 import * as btlService from "./service";
 import { response as res } from "../../../utils/response";
 
-export async function createBtl(context: AlcoCTXs["create"]) {
+export async function createBtl(context: BottleCTXs["create"]) {
   const { body, params, authPayload } = context;
 
   const bottle = await btlService.createBottle(
-    authPayload.userId,
-    params.shopId,
+    { ...params, ownerId: authPayload.userId },
     body,
   );
 
   return res.ok("Bottle created.", { bottle });
 }
 
-export async function updateBtl(context: AlcoCTXs["update"]) {
+export async function updateBtl(context: BottleCTXs["update"]) {
   const { authPayload, body, params } = context;
 
   const bottle = await btlService.updateBottle(
-    authPayload.userId,
-    params.shopId,
-    params.bottleId,
+    { ...params, ownerId: authPayload.userId },
     body,
   );
 
   return res.ok("Bottle updated.", { bottle });
 }
 
-export async function deleteBtl(context: AlcoCTXs["del"]) {
+export async function deleteBtl(context: BottleCTXs["del"]) {
   const { params, authPayload } = context;
 
-  const bottle = await btlService.deleteBottle(
-    authPayload.userId,
-    params.shopId,
-    params.bottleId,
-  );
+  const bottle = await btlService.deleteBottle({
+    ...params,
+    ownerId: authPayload.userId,
+  });
 
   return res.ok("Bottle deleted.", { bottle });
 }
 
-export async function getShopBottles(context: AlcoCTXs["qAll"]) {
+export async function getShopBottles(context: BottleCTXs["qAll"]) {
   const { params, authPayload, query } = context;
 
   const bottles = await btlService.queryAll(
-    authPayload.userId,
-    params.shopId,
+    { ...params, ownerId: authPayload.userId },
     query,
   );
 
   return bottles;
 }
 
-export async function getBtlById(context: AlcoCTXs["qOne"]) {
+export async function getBtlById(context: BottleCTXs["qOne"]) {
   const { authPayload, params } = context;
 
-  const bottle = await btlService.queryById(
-    authPayload.userId,
-    params.shopId,
-    params.bottleId,
-  );
+  const bottle = await btlService.queryById({
+    ...params,
+    ownerId: authPayload.userId,
+  });
 
   return res.ok("Bottle fetched.", {
     bottle,
   });
+}
+
+// ------------------- Bottle Lot -------------------
+export async function createBtlLot(context: BottleCTXs["createLot"]) {
+  const { params, body, authPayload } = context;
+
+  const lot = await btlService.createBtlLot(
+    { ...params, ownerId: authPayload.userId },
+    body,
+  );
+
+  return res.ok("Lot created.", { lot });
+}
+
+export async function updateBtlLot(context: BottleCTXs["updateLot"]) {
+  const { params, body, authPayload } = context;
+
+  const lot = await btlService.updateBtlLot(
+    { ...params, ownerId: authPayload.userId },
+    body,
+  );
+
+  return res.ok("Lot updated.", { lot });
+}
+
+export async function deleteBtlLot(context: BottleCTXs["deleteLot"]) {
+  const { params, authPayload } = context;
+
+  const lot = await btlService.deleteBtlLot({
+    ...params,
+    ownerId: authPayload.userId,
+  });
+
+  return res.ok("Lot deleted.", { id: lot.id });
 }
