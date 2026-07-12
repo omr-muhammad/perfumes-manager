205fcff37773995b06fde197476507a70aa3fc87
omar <omarelsayedmuhammad@gmail.com>
2026-07-12 05:15:58 +0300

    add: Completed layout header

--- packages/frontend/src/ui/Header.tsx
+++ packages/frontend/src/ui/Header.tsx
@@ -1,20 +1,42 @@
 import styles from "./app-layout.module.css";
-import { useLogout } from "../features/Auth/hooks";
+import { loggedUserQuery, useLogout } from "../features/Auth/hooks";
 import { Spinner } from "./Spinner";
-import { CiLogout, CiUser } from "react-icons/ci";
 import { Link } from "react-router";
+import { ThemeToggler } from "./ThemeToggler";
+import { LuLogOut, LuUser } from "react-icons/lu";
+import { useQuery } from "@tanstack/react-query";
 
 export function Header() {
   const { logout, loggingOut } = useLogout();
+  const { data: user } = useQuery(loggedUserQuery)
 
   return (
     <header className={styles.header}>
-      <Link to="/dashboard/profile">
-        <CiUser size={22} />
-      </Link>
-      <button onClick={() => logout()} disabled={loggingOut}>
-        {loggingOut ? <Spinner size="1rem" /> : <CiLogout className={styles.logoutIcon} size={22} />}
-      </button>
+      {
+        user !== undefined &&
+        <div className={styles.wrapper}>
+          <img className={styles.avatar} src="/default-avatar.png" alt={`Avatar of ${user.name}`} />
+          <span>{user.username}</span>
+        </div>
+      }
+
+      <ul className={styles.headerMenu}>
+        <li>
+          <Link to="/dashboard/profile">
+            <LuUser size={20} />
+          </Link>
+        </li>
+        <li>
+          <ThemeToggler wrap={false} />
+        </li>
+        <li>
+          {loggingOut ?
+            <Spinner size="1rem" />
+            :
+            <LuLogOut onClick={() => logout()} className={styles.logoutIcon} size={20} />
+          }
+        </li>
+      </ul>
     </header>
   );
 }
