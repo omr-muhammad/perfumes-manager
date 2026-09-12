c49e643188420f72cd030e0f34e5d9ee2367efa0
omar <omarelsayedmuhammad@gmail.com>
2026-09-12 14:20:10 +0300

    add: Finished perfumes compounds api callers

--- packages/frontend/src/api/compoundsAPI.ts
+++ packages/frontend/src/api/compoundsAPI.ts
@@ -10,14 +10,53 @@ export type CompoundsGetResponse = NonNullable<
   Treaty.Data<typeof backend.api.compounds.get>["data"]
 >["compounds"]["data"];
 
+// -------------------- Create Types --------------------
+type NewCompound = Parameters<typeof backend.api.compounds.post>[0];
+
+// -------------------- Update Types --------------------
+type UpdateCompound = Parameters<
+  ReturnType<typeof backend.api.admin.compounds>["patch"]
+>[0];
+
 export async function apiGetCompounds(query: CompoundsQuery) {
   const { data, error } = await backend.api.compounds.get({ query });
-
-  console.log("Api Error: ", error?.value ?? "no value");
-  console.log("API Data: ", data);
 
   if (error || !data.success)
     throw new Error(error?.value.message || data?.message);
 
   return data.data!.compounds;
+}
+
+export async function apiCreateCompound(newComp: NewCompound) {
+  const { data, error } = await backend.api.compounds.post(newComp);
+
+  if (error || !data.success)
+    throw new Error(error?.value.message || data?.message);
+
+  return data.data!.perfumeComp;
+}
+
+export async function apiUpdateCompound(
+  compoundId: number,
+  updates: UpdateCompound,
+) {
+  const { data, error } = await backend.api.admin
+    .compounds({ compoundId })
+    .patch(updates);
+
+  if (error || !data.success)
+    throw new Error(error?.value.message || data?.message);
+
+  return data.data!.perfumeComp;
+}
+
+export async function apiDeleteCompound(compoundId: number) {
+  const { data, error } = await backend.api.admin
+    .compounds({ compoundId })
+    .delete();
+
+  if (error || !data.success)
+    throw new Error(error?.value.message || data?.message);
+
+  return data.data!.perfumeCompId;
 }
